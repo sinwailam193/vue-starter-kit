eff2c34872b5b3bc01cbbc4fddfbe6c580e1ef0b
sinwailam193 <aaronsinwailam@gmail.com>
2017-09-09 13:34:12 -0700

    Starter kit

---
+++ src/components/hello.vue
@@ -0,0 +1,16 @@
+<template lang="html">
+    <div class="hello">
+        <img src="../assets/logo.png">
+        <h1>{{ test }} World</h1>
+    </div>
+</template>
+
+<script>
+    import Vue from "vue";
+    import Component from "vue-class-component";
+
+    @Component
+    export default class Hello extends Vue {
+        test = "Hello";
+    }
+</script>
